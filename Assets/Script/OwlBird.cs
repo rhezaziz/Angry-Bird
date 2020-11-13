b6d4f7ad51095c0c3c98f7d525c619c9f35ad747
rhezaziz <rheza.khairullahaziz10@gmail.com>
2020-11-13 20:27:37 +0700

    first commit

---
+++ Assets/Script/OwlBird.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    public bool _boom = false;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!_boom)
+        {
+            Debug.Log("boom");
+            StartCoroutine(Kaboom(2));
+        }
+        
+    }
+    private IEnumerator Kaboom(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Collider.radius = 1.5f;
+    }
+}
---
+++ Assets/Script/YellowBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBird : Bird
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public float _boostForce = 100;
+    public bool _hasBoost = false;
+
+    public void Boost()
+    {
+        Debug.Log("Booooost");
+        if (State == BirdState.Thrown && !_hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * _boostForce);
+            _hasBoost = true;
+        }
+    }
+
+    //public virtual void OnTap()
+    //{
+    //    
+    //}
+    public override void OnTap()
+    {
+        Boost();
+    }
+}
